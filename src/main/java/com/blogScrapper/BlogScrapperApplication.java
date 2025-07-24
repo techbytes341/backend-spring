0e571f4a239ef6326e11b2a10c9de10ede36a661
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2025-07-24 13:41:18 +0530

    initialized - backend spring api

---
+++ src/main/java/com/blogScrapper/BlogScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.blogScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogScrapper/BlogScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogScrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
